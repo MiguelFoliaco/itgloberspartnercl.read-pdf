0ee4e678668078b2c6d02fdb40353ae05637662e
Miguel Foliaco <foliaco18@outlook.es>
2022-11-29 09:45:24 -0500

    FEAT(READ PDF)

---
+++ react/components/PDFReader.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+
+type props = {
+  pdfUrl: string;
+  width: string;
+  height: string;
+}
+
+export const PDFReader = ({ height, pdfUrl, width }: props) => {
+
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+
+  return (
+    <>
+      {
+        mounted && (
+          <div className='flex justify-center'>
+            <object
+              data={pdfUrl}
+              type='application/pdf'
+              width={width}
+              height={height}
+            >
+              <iframe title='PDF View' src={pdfUrl} width={width} height={height}>
+                <p>Asegurese que el pdf exista o que el navegador soporte PDF</p>
+              </iframe>
+            </object>
+          </div>
+        )
+      }
+    </>
+  )
+}
